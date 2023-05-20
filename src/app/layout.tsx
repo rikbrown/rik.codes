929d3fa0649bdd35adb33f096cc53eab006b04b4
Rik Brown <rik@rik.codes>
2023-05-20 14:17:54 -0400

    WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,23 @@ import "./tailwind.css"
 import { Inter } from "next/font/google"
 import { ReactNode } from "react"
 import clsx from "clsx"
+import { Metadata } from "next"
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Rik Brown | Lead Software Engineer",
   description: "Experienced lead engineer in backend, frontend and mobile environments",
+  icons: {
+    icon: "/favicon/favicon-32x32.png",
+    shortcut: "/favicon/android-chrome-192x192.png",
+    apple: "/favicon/apple-touch-icon.png",
+  },
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html className=" h-full antialiased" lang="en">
+    <html className="h-full antialiased" lang="en">
       <body className={clsx("flex h-full flex-col bg-zinc-50  text-zinc-600 dark:bg-black dark:text-zinc-400 ", inter.className)}>
         <div className="fixed inset-0 flex justify-center px-8">
           <div className="flex w-full max-w-7xl bg-white p-8 ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20">{children}</div>
---
+++ src/components/motionWrappers.ts
@@ -0,0 +1,9 @@
+"use client"
+
+import { motion } from "framer-motion"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Link from "next/link"
+
+export const MotionWrappers = motion(FontAwesomeIcon)
+
+export const MotionLink = motion(Link)
